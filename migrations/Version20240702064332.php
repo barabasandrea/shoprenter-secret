678438812edbab5936dd5a61730908dba72d1bc3
barabasandrea <kicsiannacska@gmail.com>
2024-07-03 18:50:42 +0300

    v1

--- migrations/Version20240702064332.php
+++ migrations/Version20240702064332.php
@@ -20,7 +20,7 @@ final class Version20240702064332 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE secret (id SERIAL PRIMARY KEY) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci');
+        $this->addSql('CREATE TABLE secret (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
 
     public function down(Schema $schema): void
